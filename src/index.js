0ff0c11547f72be99e272eba703c5bdbf018e9c3
bigpimplimp <levi.jcecil@gmail.com>
2025-06-04 13:19:32 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './style.css';
 
 console.log('Connected');
+
+class HashMap {
+  constructor(loadFactor, capacity) {
+      this.loadFactor = loadFactor;
+			this.capacity = capacity;
+    }
+
+  hash(key) {
+    let hashCode = 0;
+       
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    } 	
+ 
+    return hashCode % 2;
+  } 
+
+	set(key, value) {
+		const hashKey = hash(key);
+
+
+		
+	}
+
+	get(key) {
+		
+	}
+	
+}
