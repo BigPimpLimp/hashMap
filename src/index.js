b25f3ee9431bc38d0a21fbc66a2c35f61681b212
BigPimpLimp <121207540+BigPimpLimp@users.noreply.github.com>
2025-06-05 00:53:17 -0400

    :)

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import './style.css';
+// import './style.css';
 
 console.log('Connected');
 
 class HashMap {
-  constructor(loadFactor, capacity) {
-      this.loadFactor = loadFactor;
-			this.capacity = capacity;
+  constructor(capacity = Array(16).fill(null)) {
+      this.loadFactor = .8;
+			this.capacity = capacity.fill(null);
+      this.entries = 0;
     }
 
   hash(key) {
@@ -15,14 +16,25 @@ class HashMap {
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
     } 	
- 
-    return hashCode % 2;
+    console.log(hashCode);
+    return hashCode % this.capacity.length;
   } 
 
 	set(key, value) {
-		const hashKey = hash(key);
-
-
+    
+		let index = this.hash(key);
+    console.log(index);
+    this.entries++;
+    console.log(`Amount of entries: ${this.entries}`)
+    if (this.entries > (this.capacity * this.loadFactor)) {
+      console.log('here');
+    }
+    if (this.capacity[index]) {
+      console.log('value is already here')
+      
+    }
+    this.capacity[index] = value;
+    console.log(this.capacity[index]);
 		
 	}
 
@@ -30,4 +42,14 @@ class HashMap {
 		
 	}
 	
+  getCap() {
+    return this.capacity;
+  }
 }
+
+const test = new HashMap();
+console.log(test.getCap());
+test.set('Levi', 'orange');
+test.set('Levi', 'glue');
+test.set('Leviia42345', 'orange');
+console.log(test.getCap());
